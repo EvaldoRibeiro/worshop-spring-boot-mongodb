62a5ba45fcbc84e53f587ce067c3a6b01f26c7b3
Evaldo Ribeiro <evardis@gmail.com>
2020-07-30 16:14:17 -0300

    Consulta simples com query methods

--- src/main/java/com/educandoweb/workshopmongo/services/PostService.java
+++ src/main/java/com/educandoweb/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.educandoweb.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,9 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 
+	}
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 
 }
