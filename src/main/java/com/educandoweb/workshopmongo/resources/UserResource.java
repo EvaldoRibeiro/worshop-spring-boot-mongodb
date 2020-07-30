498b683c3bdb6e71b0fa27231e3e859da57e20a1
Evaldo Ribeiro <evardis@gmail.com>
2020-07-30 01:00:35 -0300

    Atualização de usuário com PUT

--- src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
@@ -19,33 +19,44 @@ import com.educandoweb.workshopmongo.dto.UserDTO;
 import com.educandoweb.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping (value = "/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
-	@RequestMapping(method = RequestMethod.GET) // ou GetMapping  
-	public ResponseEntity<List<UserDTO>> findAll(){
+
+	@RequestMapping(method = RequestMethod.GET) // ou GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	@RequestMapping(value ="/{id}", method=RequestMethod.GET) // ou GetMapping  
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET) // ou GetMapping
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-	@PostMapping		//@RequestMapping(method=RequestMethod.POST)   
-	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+
+	@PostMapping // @RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	@RequestMapping(value ="/{id}", method=RequestMethod.DELETE)   
-	public ResponseEntity<Void> delete(@PathVariable String id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method=RequestMethod.PUT)    
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 }
